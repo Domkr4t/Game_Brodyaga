593baaf5893a1d1342cf8e31a5c98da6ed02fed5
Domkr4t <56762351+Domkr4t@users.noreply.github.com>
2024-03-06 18:46:13 +0300

    added health and mana, added lose screen, added change difficult, added the ability to break down walls

--- Game_Brodyaga/Program.cs
+++ Game_Brodyaga/Program.cs
@@ -7,13 +7,15 @@ namespace Game_Brodyaga
     {
         static void Main(string[] args)
         {
-            int mapNumber = 1;
+            int mapNumber = 1, difficult = 1;
             while (true)
             {
-                Game(mapNumber);
+                Game(mapNumber, difficult);
+
                 Console.WriteLine("\n\n1. Чтобы перезапустить игру нажмите R" +
                     "\n2. Чтобы выбрать другой уровень нажмите S" +
-                    "\n3. Чтобы выйти из игры нажмите Esc!");
+                    "\n3. Чтобы выбрать уровень сложности нажмите H" +
+                    "\n4. Чтобы выйти из игры нажмите Esc!");
                 ConsoleKeyInfo key = Console.ReadKey();
                 if (key.Key == ConsoleKey.R)
                 {
@@ -27,6 +29,14 @@ namespace Game_Brodyaga
                     Console.Clear();
                     Console.Write("Введите номер уровня (от 1 до 2): ");
                     mapNumber = Convert.ToInt32(Console.ReadLine());
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                }
+                else if (key.Key == ConsoleKey.H)
+                {
+                    Console.Clear();
+                    Console.Write("Введите сложность (от 1 до 3): ");
+                    difficult = Convert.ToInt32(Console.ReadLine());
                     Console.ForegroundColor = ConsoleColor.White;
                     Console.Clear();
                 }
@@ -78,7 +88,7 @@ namespace Game_Brodyaga
             Console.Write(bar);
             Console.BackgroundColor = ConsoleColor.Black;
 
-            bar = " ";
+            bar = "";
 
             for (int i = value; i < maxValue; i++)
             {
@@ -89,17 +99,40 @@ namespace Game_Brodyaga
 
         }
 
-        public static void Game(int mapNumber = 1)
+        public static void Game(int mapNumber = 1, int difficult = 1)
         {
             Console.Title = "Бродилка";
             bool isOpen = true;
             Console.CursorVisible = false;
+            Console.WindowHeight = 30;
+            Console.WindowWidth = 140;
 
-            char[,] map = ReadMap($@"C:\Projects\Game_Brodyaga\Game_Brodyaga\bin\Debug\map{mapNumber}.txt");
+            char[,] map = ReadMap($@"C:\Projects\Test\Test\bin\Debug\map{mapNumber}.txt");
 
             Random rnd = new Random();
 
-            int userX = 0, userY = 0, score = 0, countOfX = rnd.Next(1, 11);
+            int userX = 0, userY = 0, score = 0, countOfX = rnd.Next(1, 11),
+                startHealth = 3,
+                userHealth = 0,
+                startMana = 2,
+                userMana = 0;
+
+            switch (difficult)
+            {
+                case 1:
+                    userHealth = 3;
+                    userMana = 2;
+                    break;
+                case 2:
+                    userHealth = 2;
+                    userMana = 1;
+                    break;
+                case 3:
+                    userHealth = 1;
+                    userMana = 0;
+                    break;
+            }
+            bool isWin = true;
 
             for (int i = 0; i < countOfX; i++)
             {
@@ -123,19 +156,27 @@ namespace Game_Brodyaga
                 userY = rnd.Next(1, map.GetLength(1));
             }
 
-
             while (isOpen)
             {
                 Console.SetCursorPosition(0, 0);
                 Console.WriteLine("Вы участвуете в игре вам необходимо собрать все клады (X) " +
-                "пройдя по лабиринту!");
+                "пройдя по лабиринту! У вас есть здоровье и мана, если здоровье опуститься ниже 0, " +
+                "то вы проиграете, с помощью маны можно ломать стены, кроме внешних, " +
+                "при этом ваше здоровье не падает. Удачи!");
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine($"Уровень № {mapNumber}");
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.SetCursorPosition(0, 22);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($"Сложность № {difficult}");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(0, 27);
                 DrawBar(score, countOfX, ConsoleColor.Yellow, "Количество собранных кладов");
+                Console.SetCursorPosition(0, 25);
+                DrawBar(userMana, startMana, ConsoleColor.Blue, "Ваша мана");
+                Console.SetCursorPosition(0, 23);
+                DrawBar(userHealth, startHealth, ConsoleColor.Green, "Ваше здоровье");
 
-                Console.SetCursorPosition(0, 3);
+                Console.SetCursorPosition(0, 5);
                 for (int i = 0; i < map.GetLength(0); i++)
                 {
                     for (int j = 0; j < map.GetLength(1); j++)
@@ -152,7 +193,7 @@ namespace Game_Brodyaga
                     Console.WriteLine();
                 }
 
-                Console.SetCursorPosition(userY, userX + 3);
+                Console.SetCursorPosition(userY, userX + 5);
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write('@');
                 Console.ForegroundColor = ConsoleColor.White;
@@ -161,15 +202,68 @@ namespace Game_Brodyaga
                 {
                     case ConsoleKey.UpArrow:
                         if (map[userX - 1, userY] != '#') userX--;
+                        else if (map[userX - 1, userY] == '#')
+                        {
+                            bool isEdgeWallUp = userX - 1 < 1;
+
+                            if (!isEdgeWallUp && userMana != 0)
+                            {
+
+                                map[userX - 1, userY] = ' ';
+                                userMana--;
+
+                            }
+                            else userHealth--;
+                        }
                         break;
                     case ConsoleKey.DownArrow:
                         if (map[userX + 1, userY] != '#') userX++;
+                        else if (map[userX + 1, userY] == '#')
+                        {
+                            bool isEdgeWallDown = userX + 1 == map.GetLength(0) - 1;
+
+                            if (!isEdgeWallDown && userMana != 0)
+                            {
+
+                                map[userX + 1, userY] = ' ';
+                                userMana--;
+
+                            }
+                            else userHealth--;
+                        }
                         break;
                     case ConsoleKey.LeftArrow:
                         if (map[userX, userY - 1] != '#') userY--;
+                        else if (map[userX, userY - 1] == '#')
+                        {
+                            bool isEdgeWallLeft = userY - 1 < 1;
+
+                            if (!isEdgeWallLeft && userMana != 0)
+                            {
+
+
+                                map[userX, userY - 1] = ' ';
+                                userMana--;
+
+                            }
+                            else userHealth--;
+                        }
                         break;
                     case ConsoleKey.RightArrow:
                         if (map[userX, userY + 1] != '#') userY++;
+                        else if (map[userX, userY + 1] == '#')
+                        {
+                            bool isEdgeWallRight = userY + 1 == map.GetLength(1) - 1;
+
+                            if (!isEdgeWallRight && userMana != 0)
+                            {
+
+                                map[userX, userY + 1] = ' ';
+                                userMana--;
+
+                            }
+                            else userHealth--;
+                        }
                         break;
 
                 }
@@ -184,15 +278,30 @@ namespace Game_Brodyaga
                 if (score == (countOfX))
                 {
                     isOpen = false;
+                    isWin = true;
                 }
+                else if (userHealth == 0)
+                {
+                    isOpen = false;
+                    isWin = false;
+                }
+
 
                 Console.Clear();
             }
-
             Console.Clear();
             Console.SetCursorPosition(0, 0);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("__  __   ____    __  __        _       __    ____    _   __\r\n\\ \\/ /  / __ \\  / / / /       | |     / /   /  _/   / | / /\r\n \\  /  / / / / / / / /        | | /| / /    / /    /  |/ / \r\n / /  / /_/ / / /_/ /         | |/ |/ /   _/ /    / /|  /  \r\n/_/   \\____/  \\____/          |__/|__/   /___/   /_/ |_/   \r\n                                                           ");
+
+            if (isWin == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("__  __   ____    __  __        _       __    ____    _   __\r\n\\ \\/ /  / __ \\  / / / /       | |     / /   /  _/   / | / /\r\n \\  /  / / / / / / / /        | | /| / /    / /    /  |/ / \r\n / /  / /_/ / / /_/ /         | |/ |/ /   _/ /    / /|  /  \r\n/_/   \\____/  \\____/          |__/|__/   /___/   /_/ |_/   \r\n                                                           ");
+            }
+            else if (isWin == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("__  __   ____    __  __           __    ____    _____    ______\r\n\\ \\/ /  / __ \\  / / / /          / /   / __ \\  / ___/   / ____/\r\n \\  /  / / / / / / / /          / /   / / / /  \\__ \\   / __/   \r\n / /  / /_/ / / /_/ /          / /___/ /_/ /  ___/ /  / /___   \r\n/_/   \\____/  \\____/          /_____/\\____/  /____/  /_____/   \r\n                                                               ");
+            }
         }
     }
 }
